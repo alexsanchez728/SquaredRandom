6764aeefa7631bcd40d33d6babec7f1fe50ce884
Alex Sanchez <alesanchez728@gmail.com>
2018-01-21 14:23:16 -0600

    even squares 1-50 print

--- SquaredRandom/Program.cs
+++ SquaredRandom/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SquaredRandom.Terminal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace SquaredRandom
     {
         static void Main(string[] args)
         {
+            SquareToEven.GetEvenSquares();
         }
     }
 }
---
+++ SquaredRandom/Terminal/SquareToEven.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SquaredRandom.Terminal
+{
+    class SquareToEven
+    {
+        static public void GetEvenSquares()
+        {
+            Random rnd = new Random();
+            var numThings = new List<int>();
+            var squareThings = new List<double>();
+            var onlyEvens = new List<double>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                var rndValue = rnd.Next(1, 50);
+                numThings.Add(rndValue);
+                Console.WriteLine($"Number 1-50: {rndValue}");
+            }
+
+            foreach(var thing in numThings)
+            {
+                var roundSquare = Math.Round(Math.Sqrt(thing));
+                squareThings.Add(roundSquare);
+                Console.WriteLine($"square of Number: {roundSquare}");
+            }
+
+            foreach (var numb in squareThings)
+            {
+                var numToEven = numb;
+                if (numToEven % 2 == 0)
+                {
+                    onlyEvens.Add(numToEven);
+                    Console.WriteLine($"Only Even square numbers: {numToEven}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
